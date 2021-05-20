8b18a14460b07feb13663622a61b57d0d8264095
Mark Wong <markmcwong@yahoo.com>
2021-05-21 05:31:09 +0800

    tweak .travis.yml

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
+  // we should collect coverage
+  collectCoverage: true,
+  // set a directory for coverage cache
+  // coverageDirectory: '<rootDir>/tests/__coverage__',
+  // set patterns to ignore for coverage
+  coveragePathIgnorePatterns: ['/node_modules/']
 };
