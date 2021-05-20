6b67842eb21164567416167b5d82674c0b7f3d29
karenlaw-0316 <lawhiuyankarenlaw@gmail.com>
2021-05-21 04:23:48 +0800

    tweak travis.yml

--- firebaseEmulator.sh
+++ firebaseEmulator.sh
@@ -1,5 +1,5 @@
 firebase emulators:start --token 1//0geD3sD-B0sdmCgYIARAAGBASNwF-L9IrseqfihLsr1syZ-JhZxBkYBx1bW5ES6jlocLhjyC0AuglV9TlXvyWRLIgITtJpzWx0j0 &
 P1=$!
-sleep 10 & P2=$!
+sleep 15 & P2=$!
 wait $P2
 npm run test:unit
